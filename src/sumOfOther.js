28fd5188fc07aa2a0a1708e41795c23c89defc5e
Nick Levkovich <nicklevkovic@gmail.com>
2018-10-28 19:35:52 +0300

    refactor

--- src/sumOfOther.js
+++ src/sumOfOther.js
@@ -1,10 +1,9 @@
 module.exports = function sumOfOther(arr) {
   let tempArr = arr.slice();
   const result = [];
-  const reducer = (prevVal, curVal) => prevVal + curVal;
   for (let i = 0; i < arr.length; i += 1) {
     tempArr.splice(i, 1);
-    result.push(tempArr.reduce(reducer));
+    result.push(tempArr.reduce((acc, item) => acc + item));
     tempArr = arr.slice();
   }
   return result;
